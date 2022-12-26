78c4dbc6b8e39df2a2c7e0136fcac4d4c0e0da23
fastcampus-project-board <damhee6624@naver.com>
2022-12-27 08:55:07 +0900

    springapplication run

---
+++ src/main/java/hello/springmvc/basic/thymeleaf/ThymeleafBasicApplication.java
@@ -0,0 +1,57 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/hello-basic")
+    public String helloBasic(){
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 어노테이션 (코드보기)
+     * @GetMApping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+    @GetMapping(value = "mapping-get-v2")
+    public String  mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    /**
+     * pathvariable 사용
+     * 변수명이 같으면 생략 가능
+     * @pathvariable("userid") string userid -> @pathvariable userid
+     * /mapping/usera
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId = {}", data);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId){
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+}
+
